4d14bd4589b4aa1e3faac5ffa310a34c4211871b
Michał Lewandowski <m.levvy@gmail.com>
2014-01-06 16:23:14 +0100

    Zmiana roku na 2014

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#count").countdown({
 		//to change lunch date just replace the current date with yours .
-		date: "october 12, 2013, 08:00",
+		date: "september 26, 2014, 09:00",
 		//html code in count div here.
 		htmlTemplate: $('#count').html()
 	});
