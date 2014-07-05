633cbd3555c8ebe54476acc8f48a746287899113
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2014-07-05 14:13:32 +0200

    Added new way of handling js dependencies.

--- app/static/javascripts/defer-call-for-papers.js
+++ app/static/javascripts/defer-call-for-papers.js
@@ -1,29 +1,16 @@
 (function loadDefferdJs() {
-    function getScript(url, success) {
-        var script = document.createElement('script');
-        script.src = url;
-        var head = document.getElementsByTagName('head')[0],
-            done = false;
-        script.onload = script.onreadystatechange = function () {
-            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-                done = true;
-                success();
-                script.onload = script.onreadystatechange = null;
-                head.removeChild(script);
+    yepnope({
+        load: [
+            'static/javascripts/vendor/jquery.js',
+            'static/javascripts/foundation/foundation.js',
+            'static/javascripts/foundation/foundation.accordion.js',
+            'static/javascripts/google-analytics.js'
+        ],
+        callback: {
+            'foundation.accordion.js':function() {
+                $(document).foundation();
             }
-        };
-        head.appendChild(script);
-    }
-
-    getScript('static/javascripts/vendor/require.js', function () {
-        require(['static/javascripts/defer'], function () {
-            require(['static/javascripts/vendor/jquery'], function () {
-                require(['static/javascripts/foundation/foundation'], function () {
-                    require(['static/javascripts/foundation/foundation.accordion'], function () {
-                    });
-                });
-            });
-        });
+        }
     });
 })();
 
