8869bbcd18b4f6af7800b527ff5fc08a56548cb4
Michał Lewandowski <mlw@touk.pl>
2014-07-15 20:16:32 +0200

    Deploy script to handle prod and staging deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker stop warsjawa
-docker rm warsjawa
-docker build -t warsjawa .
-docker run --name warsjawa --link db:db -d -p 80:80 -p 81:81 warsjawa
+docker stop $3
+docker rm $3
+docker build -t $3 .
+docker run --name $3 --link $4:db -d -p 80:$1 -p 81:$2 warsjawa
