34da068b8fdabc819c50e712abd8cd437deda387
Michał Lewandowski <mlw@touk.pl>
2014-07-16 18:44:40 +0200

    New speaker Zapier auth

--- app/backend/index.py
+++ app/backend/index.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask import json
+from flask import Flask, json, request, Response
 from bson import json_util
 from pymongo import MongoClient
+from functools import wraps
+import os
 
 app = Flask(__name__)
 
@@ -11,10 +12,30 @@ db = client.warsjawa
 def toJson(data):
     return json.dumps(data, default=json_util.default)
 
+def check_auth(username, password):
+    return username == os.environ.get('HTTP_USERNAME') and password == os.environ.get('HTTP_PASS')
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return Response("Not authorized", 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/api/speakers')
-def test_json():
+def speakers():
     speakers_docs = [doc for doc in db.speakers.find({},{"_id":0})]
     return toJson(speakers_docs)
+
+@app.route('/api/speakers', methods=['POST'])
+@requires_auth
+def newSpeaker():
+	#TODO Save to DB
+	#TODO Send Email
+    return toJson({"status":"done"})
+
 
 if __name__ == "__main__":
     app.config['DEBUG'] = True
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 docker stop $3
 docker rm $3
 docker build -t $3 .
-docker run --name $3 --link $4:db -d -p $1:80 -p $2:81 $3
+docker run --name $3 --link $4:db -d -p $1:80 -p $2:81 -e HTTP_USERNAME=$5 -e HTTP_PASS=$6 $3
